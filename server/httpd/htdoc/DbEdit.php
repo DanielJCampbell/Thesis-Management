d82ba78584b52609f2bd611aa8e16854956ae98c
Tomas Cantwell <mossman2142@gmail.com>
2014-07-31 11:59:29 +1200

    select works and insert does not

--- server/httpd/htdoc/DbEdit.php
+++ server/httpd/htdoc/DbEdit.php
@@ -10,9 +10,19 @@ $db = new mysqli($location, $username, $password, $database);
 if($db->connect_errno > 0){
     die('Unable to connect to database [' . $db->connect_error . ']');
 }
-$query = "INSERT INTO Testing (\"L_Name\",\"F_Name\",\"ID\") VALUES(\"derp\",\"Mc man\",3),(\"hello\",\"world\",6);";
- mysqli_query ( $db, $query);
-
+/* Create table doesn't return a resultset */
+if (mysqli_query($db, "INSERT INTO Testing L_Name VALUES(Derp)") === TRUE) {
+    printf("Data inserted successfully.\n");
+}else{
+	printf("something went wrong inserting\n");
+}
+$try = mysqli_query($db,"INSERT INTO Testing (`L_Name`, `F_Name`, `ID`) VALUES ('guy','lolz',7)"); //valid insert code for db but not vaild in mysqli 
+//php
+if($try){
+printf("it worked\n");
+}else{
+printf("it failed\n");
+}
 $db->close();
 ?>
 
