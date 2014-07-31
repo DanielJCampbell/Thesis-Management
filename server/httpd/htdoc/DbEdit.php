66fa7e5920c092a1852e0efc0b9bc40967b61912
Tomas Cantwell <mossman2142@gmail.com>
2014-07-31 15:13:59 +1200

    something

--- server/httpd/htdoc/DbEdit.php
+++ server/httpd/htdoc/DbEdit.php
@@ -10,13 +10,15 @@ $db = new mysqli($location, $username, $password, $database);
 if($db->connect_errno > 0){
     die('Unable to connect to database [' . $db->connect_error . ']');
 }
-/* Create table doesn't return a resultset */
+
 if (mysqli_query($db, "INSERT INTO Testing L_Name VALUES(Derp)") === TRUE) {
     printf("Data inserted successfully.\n");
 }else{
 	printf("something went wrong inserting\n");
 }
-$try = mysqli_query($db,"INSERT INTO Testing (`L_Name`, `F_Name`, `ID`) VALUES ('guy','lolz',7)"); //valid insert code for db but not vaild in mysqli 
+mysqli_select_db($db,"Testing");
+$query_string = "INSERT INTO `Testing` (`L_Name`, `F_Name`, `ID`) VALUES ('guy','lolz',7)";
+$try = mysqli_query($db,$query_string,1); //valid insert code for db but not vaild in mysqli 
 //php
 if($try){
 printf("it worked\n");
