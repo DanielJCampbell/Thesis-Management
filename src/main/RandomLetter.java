257962564fa41c7bb86ba8a066f6587d127d8b86
samuraiRAZR <alvinpascual21@ymail.com>
2014-07-31 16:17:02 +1200

    Added Random Letter

---
+++ src/main/RandomLetter.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Random;
+
+public class RandomLetter {
+	
+	 public String symbols;
+
+	  public RandomLetter(){
+		  final String alphabet = "ID";
+		    final int N = alphabet.length();
+
+		    Random r = new Random();
+
+		    for (int i = 0; i < 50; i++) {
+		        symbols =  Character.toString(alphabet.charAt(r.nextInt(N)));
+		    }
+	  } 
+
+}
