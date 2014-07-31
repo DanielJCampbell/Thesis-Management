4d5d4906d737b7cee04c7755cbbf6beb6b290fba
samuraiRAZR <alvinpascual21@ymail.com>
2014-07-31 16:16:41 +1200

    Test Data generator ready for use

--- src/main/MyWriter.java
+++ src/main/MyWriter.java
@@ -10,9 +10,17 @@ public class MyWriter {
 		String lname = new RandomString().symbols;
 		boolean halftime = new RandomBoolean().symbols;
 		int studentid = 300000000 + i;
+		String degree = new RandomString().symbols;
+		int supid1 = 80000000 + i;
+		int supid2 = 80000000 + i+ 300;
+		String scholar = new RandomString().symbols;
+		String notes = new RandomString().symbols;
+		String origin = new RandomLetter().symbols;
+		
+		
 		System.out.println(halftime);
 		
-		writer.println("INSERT INTO Students Values ("+fname+","+ lname +","+ halftime+ ","+ studentid + ");");
+		writer.println("INSERT INTO Students Values ("+fname+","+ lname +","+ halftime+ ","+ studentid +","+degree+","+supid1+","+supid2+","+scholar+","+notes+","+ origin+ ");");
 		}
 	}
 }
