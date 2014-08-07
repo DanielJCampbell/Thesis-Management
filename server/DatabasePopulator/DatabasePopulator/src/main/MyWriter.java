40e50df17bfb8f55272db106e01c60093c248c0e
samuraiRAZR <alvinpascual21@ymail.com>
2014-08-07 15:24:54 +1200

    work hours now add up to 450, hopefully :)

--- server/DatabasePopulator/DatabasePopulator/src/main/MyWriter.java
+++ server/DatabasePopulator/DatabasePopulator/src/main/MyWriter.java
@@ -9,6 +9,8 @@ public class MyWriter {
 		RandomName name = new RandomName();
 		String fname = name.first;
 		String lname = name.last;
+		String course = new RandomString().symbols;
+		String spec = new RandomString().symbols;
 		boolean halftime = new RandomBoolean().symbols;
 		int studentid = 300000000 + i;
 
@@ -29,61 +31,62 @@ public class MyWriter {
 		RandomDate thesisM = prop;
 		RandomDate thesisP = prop;
 		thesisM.moveMonths(12);
-		thesisP.moveMonths(thesisP.randBetween(36,48));
-		String course = new RandomString().symbols;
-		String spec = new RandomString().symbols;
+		thesisP.moveMonths(RandomDate.randBetween(36,48));
+
 
 		String propSubDate = prop.symbols;
-		prop.moveMonths(prop.randBetween(1,3));
+		prop.moveMonths(RandomDate.randBetween(1,3));
 		String propDeadDate = prop.symbols;
-		prop.moveMonths(prop.randBetween(1,3));
+		prop.moveMonths(RandomDate.randBetween(1,3));
 		String propConfDate = prop.symbols;
 
 		prop.moveMonths(3);
 		String prop3mthSubDate = prop.symbols;
-		prop.moveMonths(prop.randBetween(1,3));
+		prop.moveMonths(RandomDate.randBetween(1,3));
 		String prop3mthDeadDate = prop.symbols;
-		prop.moveMonths(prop.randBetween(1,3));
+		prop.moveMonths(RandomDate.randBetween(1,3));
 		String prop3mthApprDate = prop.symbols;
 
 		prop.moveMonths(5);
 		String prop8mthDeadDate = prop.symbols;
-		prop.moveMonths(prop.randBetween(1,3));
+		prop.moveMonths(RandomDate.randBetween(1,3));
 		String prop8mthSubDate = prop.symbols;
-		prop.moveMonths(prop.randBetween(1,3));
+		prop.moveMonths(RandomDate.randBetween(1,3));
 		String prop8mthApprDate = prop.symbols;
 
 				// thesisM due 1 year from start (masters)
 				String thesisSub = thesisM.symbols;
-				thesisM.moveMonths(prop.randBetween(1,3));
+				thesisM.moveMonths(RandomDate.randBetween(1,3));
 				String thesisDead = thesisM.symbols;
-				thesisM.moveMonths(prop.randBetween(1,3));
+				thesisM.moveMonths(RandomDate.randBetween(1,3));
 				String examinersAppointed = thesisM.symbols;
-				thesisM.moveMonths(prop.randBetween(1,3));
+				thesisM.moveMonths(RandomDate.randBetween(1,3));
 				String examinationComplete = thesisM.symbols;
-				thesisM.moveMonths(prop.randBetween(1,3));
+				thesisM.moveMonths(RandomDate.randBetween(1,3));
 				String revisions = thesisM.symbols;
-				thesisM.moveMonths(prop.randBetween(1,3));
+				thesisM.moveMonths(RandomDate.randBetween(1,3));
 				String deposited = thesisM.symbols;
 
 				// thesisP due 3 years from start (PhD)
 				String thesisPSub = thesisP.symbols;
-				thesisP.moveMonths(prop.randBetween(1,3));
+				thesisP.moveMonths(RandomDate.randBetween(1,3));
 				String thesisPDead = thesisP.symbols;
-				thesisP.moveMonths(prop.randBetween(1,3));
+				thesisP.moveMonths(RandomDate.randBetween(1,3));
 				String examinersPAppointed = thesisP.symbols;
-				thesisP.moveMonths(prop.randBetween(1,3));
+				thesisP.moveMonths(RandomDate.randBetween(1,3));
 				String examinationPComplete = thesisP.symbols;
-				thesisP.moveMonths(prop.randBetween(1,3));
+				thesisP.moveMonths(RandomDate.randBetween(1,3));
 				String revisionsP = thesisP.symbols;
-				thesisP.moveMonths(prop.randBetween(1,3));
+				thesisP.moveMonths(RandomDate.randBetween(1,3));
 				String depositedP = thesisP.symbols;
 
 		//PhD
-		String degree = new RandomString().symbols;
 		String propSeminar = new RandomDate(2014,2014).symbols;
 		String fgr = new RandomDate(2014,2014).symbols;
-		String workHours = Integer.toString(135+i);
+		RandomWorkHours rwh = new RandomWorkHours();
+		String workHours1 = rwh.first;
+		String workHours2 = rwh.second;
+		String workHours3 = rwh.third;
 
 		//Supervisor
 		RandomName supervisor1 = new RandomName();
@@ -94,26 +97,9 @@ public class MyWriter {
 		String suplname2 = supervisor2.last;
 		System.out.println(fgr);
 
-		// 6mth report
-		String report6mthlyDue = "'2014-5-29'";
-		String report6mthlySub = new RandomDate(2014,2014).symbols;
-		String report6mthlyAppr = new RandomDate(2014,2014).symbols;
-
-		writer.println("INSERT INTO Students Values ("+fname+","+ lname +","+ halftime+ ","+ studentid +","+supid1+","+supPercent1 + ","+supid2+","+supPercent2+","+scholar+","+notes+","+ origin+ ");");
+		writer.println("INSERT INTO Students Values ("+fname+","+ lname +","+ course +","+spec+","+halftime+ ","+ studentid +","+supid1+","+supPercent1 + ","+supid2+","+supPercent2+","+scholar+","+notes+","+ origin+ ");");
 		writer.println("INSERT INTO Supervisors Values ("+ supfname+","+suplname+","+ supid1+");");
 		writer.println("INSERT INTO Supervisors Values ("+ supfname2+","+suplname2+","+ supid2+");");
-
-		if(i> 25 && i< 30){
-		writer.println("INSERT INTO SixMonthlyReports Values ("+studentid+","+ report6mthlyDue+","+"NULL, NULL"+");");
-		}
-		else{
-			if(i>25 &&i< 40){
-			writer.println("INSERT INTO SixMonthlyReports Values ("+studentid+","+ report6mthlyDue+","+report6mthlySub+","+report6mthlyAppr +");");
-			}
-			else if(i> 25){
-				writer.println("INSERT INTO SixMonthlyReports Values ("+studentid+","+ report6mthlyDue+","+report6mthlySub+",NULL);");
-			}
-		}
 
 		// Suspensions
 		if(i < 2){
@@ -124,15 +110,23 @@ public class MyWriter {
 		}
 
 		if(i<25){
-			writer.println("INSERT INTO MastersStudents Values ("+ studentid +","+startDate+","+course+","+spec+","+propDeadDate+","+propSubDate+","+propConfDate+","+prop3mthDeadDate+","+ prop3mthSubDate+","+prop3mthApprDate+","+ prop8mthDeadDate+","+ prop8mthSubDate+","+prop8mthApprDate+","+ thesisDead+","+thesisSub +","+ examinersAppointed+ "," +examinationComplete+"," + revisions+"," +deposited+");");
+			writer.println("INSERT INTO MastersStudents Values ("+ studentid +","+startDate+","+propDeadDate+","+propSubDate+","+propConfDate+","+prop3mthDeadDate+","+ prop3mthSubDate+","+prop3mthApprDate+","+ prop8mthDeadDate+","+ prop8mthSubDate+","+prop8mthApprDate+","+ thesisDead+","+thesisSub +","+ examinersAppointed+ "," +examinationComplete+"," + revisions+"," +deposited+");");
 		}
 		else{
 			if(i<40){
-			writer.println("INSERT INTO PhDStudents Values ("+ studentid +","+degree+","+startDate+","+propDeadDate+","+propSubDate+","+propSeminar+","+propConfDate+","+fgr+","+ thesisPDead+","+thesisPSub +","+ examinersPAppointed+ "," +examinationPComplete+"," + revisionsP+"," +depositedP+","+workHours+");");
+			writer.println("INSERT INTO PhDStudents Values ("+ studentid +","+startDate+","+propDeadDate+","+propSubDate+","+propSeminar+","+propConfDate+","+ thesisPDead+","+thesisPSub +","+ examinersPAppointed+ "," +examinationPComplete+"," + revisionsP+"," +depositedP+","+workHours1+","+workHours2+","+workHours3+");");
+			}
+			if(i<40){
+				writer.println("INSERT INTO PhDStudents Values ("+ studentid +","+startDate+","+propDeadDate+","+propSubDate+","+propSeminar+","+propConfDate+","+ thesisPDead+","+thesisPSub +","+ examinersPAppointed+ "," +examinationPComplete+"," + revisionsP+"," +depositedP+","+workHours1+","+workHours2+","+workHours3+");");
 			}
 			else{
-				writer.println("INSERT INTO PhDStudents Values ("+ studentid +","+degree+","+startDate+","+propDeadDate+","+propSubDate+","+propSeminar+","+propConfDate+","+fgr+","+ thesisPDead+","+thesisPSub +","+ examinersPAppointed+ "," +examinationPComplete+"," + revisionsP+"," +depositedP+","+"NULL"+");");
+				if(i<45){
+				writer.println("INSERT INTO PhDStudents Values ("+ studentid +","+startDate+","+propDeadDate+","+propSubDate+","+propSeminar+","+propConfDate+","+ thesisPDead+","+thesisPSub +","+ examinersPAppointed+ "," +examinationPComplete+"," + revisionsP+"," +depositedP+","+workHours1+","+workHours2+","+"NULL"+");");
 				}
+				else{
+				writer.println("INSERT INTO PhDStudents Values ("+ studentid +","+startDate+","+propDeadDate+","+propSubDate+","+propSeminar+","+propConfDate+","+ thesisPDead+","+thesisPSub +","+ examinersPAppointed+ "," +examinationPComplete+"," + revisionsP+"," +depositedP+","+"NULL,NULL,NULL"+");");
+				}
+			}
 		}
 		}
 
---
+++ server/DatabasePopulator/DatabasePopulator/src/main/RandomWorkHours.java
@@ -0,0 +1,22 @@
+package main;
+
+public class RandomWorkHours {
+
+	 public String first;
+	 public String second;
+	 public String third;
+
+	  public RandomWorkHours(){
+
+		  int value = randBetween(0,450);
+		  int value2 = randBetween(0,450-value);
+		  int value3 = 450 - (value+value2);
+		  first = Integer.toString(value);
+		  second = Integer.toString(value2);
+	  }
+
+	  public static int randBetween(int start, int end) {
+	        return start + (int)Math.round(Math.random() * (end - start));
+	    }
+
+}
