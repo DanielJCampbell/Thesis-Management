f5fe5ba6135dfe30d535419a8d7e47e15bc122a2
Tomas Cantwell <mossman2142@gmail.com>
2014-08-07 16:15:57 +1200

    student db for alvin

--- website/student.php
+++ website/student.php
@@ -21,36 +21,6 @@
     </style>
   </head>
   <body>
-  <h3> Timeline of progress:</h3>
-    <table cellpadding = "5px" cellspacing = "10px">
-    <tr>
-      <th> Start Date</th>
-      <th> Proposal</th>
-      <th> 3 Month Report </th>
-      <th> 8 Month Report </th>
-      <th> Thesis Submission </th>
-      <th> Suspension Dates </th>
-    </tr>
-    <tr>
-      <td> 5/6/2014 </td>
-      <td> 29/6/2014 </td>
-      <td> 3/9/2014 </td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    </table>
-    <br><br>
-    <h3> Upcoming Deadlines:</h3>
-    <table cellpadding = "5px" cellspacing = "10px">
-    <tr>
-      <th> 8 Month Report </th>
-      <th> Thesis Submission</th>
-    </tr>
-    <tr>
-      <td> 5/2/2015 </td>
-      <td> 5/6/2015 </td>
-    </tr>
-    </table>
+    <?php require "StudentDB.php";?>
   </body>
 </html>
