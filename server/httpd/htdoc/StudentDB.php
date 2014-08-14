6446944e1bff3ee275e5a2d687253635da9d4814
Tomas Cantwell <mossman2142@gmail.com>
2014-08-14 16:39:56 +1200

    added deadline table to student page, onlydisplays dates after current date, although not just deadlines

--- server/httpd/htdoc/StudentDB.php
+++ server/httpd/htdoc/StudentDB.php
@@ -5,6 +5,9 @@ $password = "SWEN302";
 $database = "ThesisManagement";
 $schema = array("StudentID", "Specialisation", "StartDate", "ProposalDeadline", "ProposalSubmission", "ProposalConfirmationDate", "Report3MonthDeadline", "Report3MonthSubmission", "Report3MonthApproval", "Report8MonthDeadline", "Report8MonthSubmission", "Report8MonthApproval", "ThesisDeadline", "ThesisSubmission", "ExaminersAppointedDate", "ExaminationCompleted", "RevisionsFinalised", "DepositedInLibrary");
 $schema_human_readable = array("Student\nID", "Course\nSpecialisation", "Start\nDate", "Proposal\nDeadline", "Proposal\nSubmission", "Proposal\nConfirmation", "3 Month\nReport\nDeadline", "3 Month\nReport\nSubmission", "3 Month\nreport\nApproval", "8 Month\nReport\nDeadline", "8 Month\nreport\nSubmission", "8 Month\nreport\nApproval", "Thesis\nDeadline", "Thesis\nSubmission", "Examiners\nAppointed", "Examination\nCompleted", "Revisions\nFinalised", "Deposited\nIn Library");
+
+$current_student = 300000001;
+
 //Connect to database
 $db = new mysqli($location, $username, $password, $database);
 if($db->connect_errno > 0){
@@ -15,21 +18,21 @@ if(!$db->select_db($database)){
     die('Unable to select database '.$db);
 }
     //Get the student corresponding to the entry in the MasterStudent table
-    $stud = $db->query("select * FROM Students s NATURAL JOIN MastersStudents ms WHERE s.StudentID=ms.StudentID AND s.StudentID=300000006");  
+    $stud = $db->query("select * FROM Students s NATURAL JOIN MastersStudents ms WHERE s.StudentID=ms.StudentID AND s.StudentID=" . $current_student);  
     $student = $stud->fetch_assoc();
 	echo "<table class='studentsummary' >";
-	echo "<tr style='border:none;'>";
-	echo "<td style='border:none;'><strong>Name:</strong></td>";
-	echo "<td style='border:none;'>" . $student[F_Name] . " " . $student[L_Name] . "</td>";
-	echo "</tr><tr style='border:none;'>";
-	echo "<td style='border:none;'><strong>ID:</strong></td>";
-	echo "<td style='border:none;'>" . $student[StudentID] . "</td>";
-	echo "</tr><tr style='border:none;'>";
-	echo "<td style='border:none;'><strong>Specialisation:</strong></td>";
-	echo "<td style='border:none;'>" . $student[Specialisation] . "</td>";
-	echo "</tr><tr style='border:none;'>";
-	echo "<td style='border:none;'><strong>Part Time:</strong></td>";
-	echo "<td style='border:none;'>";
+	echo "<tr>";
+	echo "<td class='studentsummary'><strong>Name:</strong></td>";
+	echo "<td class='studentsummary'>" . $student[F_Name] . " " . $student[L_Name] . "</td>";
+	echo "</tr><tr>";
+	echo "<td class='studentsummary'><strong>ID:</strong></td>";
+	echo "<td class='studentsummary'>" . $student[StudentID] . "</td>";
+	echo "</tr><tr>";
+	echo "<td class='studentsummary'><strong>Specialisation:</strong></td>";
+	echo "<td class='studentsummary'>" . $student[Specialisation] . "</td>";
+	echo "</tr><tr>";
+	echo "<td class='studentsummary'><strong>Part Time:</strong></td>";
+	echo "<td class='studentsummary'>";
     if ($student[Halftime]) echo "Yes";
     else echo "No";
 	echo "</td></tr>";
@@ -52,36 +55,44 @@ if(!$db->select_db($database)){
 	    
     $ps->close();
     $ss->close();
-    printf("<h3> Timeline of progress:</h3>");
-    echo "<table class='timeline'>";
-	echo "<tr>"; 
-    for($i =0;$i<count($schema);$i++){ 	  
-	      echo "<th>";
-	      echo $schema[$i];
-	      echo "</th>";
-    }
-	      echo "</tr>";
-	echo "<tr>";
-    for($i =0;$i<count($schema);$i++){                                                                                                                               
-                                                                                                                                                         
-              echo "<td>";                                                                                                                                           
-              echo $student[$schema[$i]];
-              echo "</td>";                                                                                                                                         
-    }
-	echo "<tr>";
+    
     echo"</table>";
     echo"<br>
 	<h3> Upcoming Deadlines:</h3>
     <table class='timeline'>
     <tr>
-      <th> 8 Month Report </th>
-      <th> Thesis Submission</th>
-    </tr>
-    <tr>
-      <td> 5/2/2015 </td>
-      <td> 5/6/2015 </td>
-    </tr>
-    </table>";
+      <th> Event </th>
+      <th> Date </th>
+    </tr>";
+    $deadLineDate = array();
+    for($i =2;$i<count($schema);$i++){ 	  
+      $tmp=$db->query("SELECT " . $schema[$i] . " FROM MastersStudents ms WHERE ms.StudentID=" . $current_student . " AND " . $schema[$i] . " >NOW()");
+      $deadLineDate[$schema[$i]]= $tmp->fetch_assoc()[$schema[$i]];
+    }
+    for($i =2;$i<count($schema);$i++){ 	  
+	$current_deadline = $deadLineDate[$schema[$i]];
+	if (!is_null($current_deadline)){
+	echo "<td ><strong>" . $schema_human_readable[$i] . "</strong></td>";
+	echo "<td>" . $current_deadline . "</td>";
+	echo "</tr><tr>";	  
+	}
+    }
+    echo"</table>";
+    
+    printf("<h3> Timeline of progress:</h3>");
+    echo "<table class='timeline'>";
+
+    echo "<th ><strong>Event</strong></th>";
+	echo "<th>Date</th>";
+	echo "</tr><tr>";
+    
+    for($i =2;$i<count($schema);$i++){ 	  
+	echo "<td ><strong>" . $schema_human_readable[$i] . "</strong></td>";
+	echo "<td>" . $student[$schema[$i]]. "</td>";
+	echo "</tr><tr>";	  
+    }
+    
+    
     $stud->close();
 
 $db->close();
