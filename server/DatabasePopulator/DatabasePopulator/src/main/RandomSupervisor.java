10b88f5096a1e6b00472ed08679b18f786c55413
samuraiRAZR <alvinpascual21@ymail.com>
2014-08-21 16:21:04 +1200

    Fixed the Supervisor percentages to add up to 100%

--- server/DatabasePopulator/DatabasePopulator/src/main/RandomSupervisor.java
+++ server/DatabasePopulator/DatabasePopulator/src/main/RandomSupervisor.java
@@ -2,20 +2,30 @@ package main;
 
 public class RandomSupervisor {
 	
-	int supid = 0;
+	int supid1 = 0;
+	int supid2 = 0;
 	String supfname = "";
 	String suplname = "";
+	String supfname2 = "";
+	String suplname2 = "";
 	String supPercent = "";
+	String supPercent2 = "";
 	
 
 	public RandomSupervisor(int i) {
 		RandomPercent rp = new RandomPercent();
 
-		supid = 80000000 + i;
+		supid1 = 80000000 + i;
+		supid2 = 80000300 + i;
 		supPercent = rp.max;
+		supPercent2 = rp.min;
 
 		RandomName supervisor = new RandomName();
+		RandomName supervisor2 = new RandomName();
 		supfname = supervisor.first;
 		suplname = supervisor.last;
+		supfname2 = supervisor2.first;
+		suplname2 = supervisor2.last;
+		
 	}
 }
