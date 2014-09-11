c9e692e65b44b271b667c3951eed78673f874352
samuraiRAZR <alvinpascual21@ymail.com>
2014-09-11 15:18:34 +1200

    Updated student to use PSQL, also updated schema slightly

--- server/httpd/htdoc/StudentDB.php
+++ server/httpd/htdoc/StudentDB.php
@@ -17,7 +17,7 @@ $schema_PhD_deadlines_hr = array("Proposal Deadline", "Thesis Deadline");
 $schema_masters_deadlines_hr = array("Proposal Deadline", "3 Month Report Deadline", "8 Month Report Deadline", "Thesis Deadline");
 
 //Connect to database
-$db = new mysqli($location, $username, $password, $database);
+$db = pg_connect("host = '".$location."' dbname = '".$database."'");
 if($db->connect_errno > 0){
     die('Unable to connect to database [' . $db->connect_error . ']');
 }
@@ -36,11 +36,11 @@ $isMasters = (int) $checkMast->fetch_assoc()[$query]; //query returns column nam
 $checkMast->close();
 
     //Get the student corresponding to the entry in the MasterStudent table
-	$stud;    
+	$stud;
 	if ($isMasters === 1)
-		$stud = $db->query("SELECT * FROM Students s NATURAL JOIN MastersStudents ms WHERE s.StudentID=" . $studentID);  
+		$stud = $db->query("SELECT * FROM Students s NATURAL JOIN MastersStudents ms WHERE s.StudentID=" . $studentID);
 	else
-		$stud = $db->query("SELECT * FROM Students s NATURAL JOIN PhDStudents ps WHERE s.StudentID=" . $studentID);  
+		$stud = $db->query("SELECT * FROM Students s NATURAL JOIN PhDStudents ps WHERE s.StudentID=" . $studentID);
     $student = $stud->fetch_assoc();
 	echo "<table class='studentsummary' >";
 	echo "<tr>";
@@ -55,24 +55,31 @@ $checkMast->close();
 	echo "</tr><tr>";
 	echo "<td class='studentsummary'><strong>Part Time:</strong></td>";
 	echo "<td class='studentsummary'>";
-    if ($student[Halftime]) echo "Yes";
+
+	$isHalftime = $db->query("SELECT * FROM EnrolmentTypeChanges WHERE StudentID = ".$row[StudentID]." ORDER BY ChangeDate DESC LIMIT 1");
+	$halftime = $isHalftime->fetch_assoc();
+	$isHalftime->close();
+
+	if ($halftime [EnrolmentType] == 'H') {
+		echo "Yes";
+	}
     else echo "No";
 	echo "</td></tr>";
-    
+
     //Query supervisors
     $ps = $db->query("SELECT * FROM Supervisors s WHERE s.SupervisorID = ".$student[Primary_SupervisorID]);
     $ss = $db->query("SELECT * FROM Supervisors s WHERE s.SupervisorID = ".$student[Secondary_SupervisorID]);
     $primary = $ps->fetch_assoc();
     $secondary = $ss->fetch_assoc();
-    
+
 	echo "<tr>";
 	echo "<td style='border:none;'><strong>Your Supervisors:</strong></td>";
 	echo "<td style='border:none;'>" . $primary[F_Name]." ".$primary[L_Name]." (".$student[Primary_SupervisorPercent]. "%)</td>";
-	echo "</tr><tr style='border:none;'>";	
+	echo "</tr><tr style='border:none;'>";
 	echo "<td style='border:none;'></td>";
 	echo "<td style='border:none;'>" . $secondary[F_Name]." \n ".$secondary[L_Name]." (".$student[Secondary_SupervisorPercent]. "%)</td>";
 	echo "</tr></table>";
-	    
+
     $ps->close();
     $ss->close();
 
@@ -85,38 +92,38 @@ $checkMast->close();
     $deadLineDate = array();
 	//list upcoming deadlines
 	if ($isMasters == 1){
-	for($i =0;$i<count($schema_masters_deadlines);$i++){ 	  
+	for($i =0;$i<count($schema_masters_deadlines);$i++){
 		$tmp=$db->query("SELECT " . $schema_masters_deadlines[$i] . " FROM MastersStudents ms WHERE ms.StudentID=" . $studentID . " AND " . $schema_masters_deadlines[$i] . " >NOW()");
 		$deadLineDate[$schema_masters_deadlines[$i]]= $tmp->fetch_assoc()[$schema_masters_deadlines[$i]];
 	}
-	for($i =0;$i<count($schema_masters_deadlines);$i++){ 	  
+	for($i =0;$i<count($schema_masters_deadlines);$i++){
 			$current_deadline = $deadLineDate[$schema_masters_deadlines[$i]];
 
 			if (!is_null($current_deadline)){
-				echo "<tr>";	
+				echo "<tr>";
 				echo "<td>\n<strong>" . $schema_masters_deadlines_hr[$i] . "</strong>\n</td>";
 				echo "<td>\n" . $current_deadline . "\n</td>";
-				echo "</tr>";	  
+				echo "</tr>";
 			}
 	}
 	}else{
-	for($i =0;$i<count($schema_PhD_deadlines);$i++){ 	  
+	for($i =0;$i<count($schema_PhD_deadlines);$i++){
 		$tmp=$db->query("SELECT " . $schema_PhD_deadlines[$i] . " FROM PhDStudents ps WHERE ps.StudentID=" . $studentID . " AND " . $schema_PhD_deadlines[$i] . " >NOW()");
 		$deadLineDate[$schema_PhD_deadlines[$i]]= $tmp->fetch_assoc()[$schema_PhD_deadlines[$i]];
 	}
-	for($i =0;$i<count($schema_PhD_deadlines);$i++){ 	  
+	for($i =0;$i<count($schema_PhD_deadlines);$i++){
 			$current_deadline = $deadLineDate[$schema_PhD_deadlines[$i]];
 
 			if (!is_null($current_deadline)){
-				echo "<tr>";	
+				echo "<tr>";
 				echo "<td>\n<strong>" . $schema_PhD_deadlines_hr[$i] . "</strong>\n</td>";
 				echo "<td>\n" . $current_deadline . "\n</td>";
-				echo "</tr>";	  
+				echo "</tr>";
 			}
 	}
 	}
     echo"</table>";
-    
+
     printf("<h3> Timeline of progress:</h3>\n");
     echo "<table id='timeline' class='timeline'>";
     echo "<tr>";
@@ -125,18 +132,18 @@ $checkMast->close();
 	echo "</tr>";
     //list all information
 	if ($isMasters == 1){
-	for($i =2;$i<count($schema_masters);$i++){ 	  
-			echo "<tr>";	
+	for($i =2;$i<count($schema_masters);$i++){
+			echo "<tr>";
 			echo "<td>\n<strong>" . $schema_masters_human_readable[$i] . "</strong>\n</td>";
 			echo "<td>\n" . $student[$schema_masters[$i]]. "\n</td>";
-			echo "</tr>";	  
+			echo "</tr>";
 	}
     }else{
-	for($i =2;$i<count($schema_PhD);$i++){ 	  
-			echo "<tr>";	
+	for($i =2;$i<count($schema_PhD);$i++){
+			echo "<tr>";
 			echo "<td>\n<strong>" . $schema_PhD_human_readable[$i] . "</strong>\n</td>";
 			echo "<td>\n" . $student[$schema_PhD[$i]]. "\n</td>";
-			echo "</tr>";	  
+			echo "</tr>";
 	}
 	}
 	echo "</table>\n";
