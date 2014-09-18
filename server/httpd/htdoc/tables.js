ed48af8b2521f13a7719707d46302897b5399644
Nainesh Patel <patelnain1@khmer.ecs.vuw.ac.nz>
2014-09-18 15:25:57 +1200

    Updated tables.js to reflect new PHP code

--- server/httpd/htdoc/tables.js
+++ server/httpd/htdoc/tables.js
@@ -1,12 +1,6 @@
 ;
-var type = "All";
-var filter = "all";
-var supervisorID = "null";
 
-function changeFilter(value) {
-    type = value;
-    sendPHPRequest();
-}
+window.onload = sendPHPRequest();
 
 function sendPHPRequest() {
   var req = new XMLHttpRequest();
@@ -15,51 +9,62 @@ function sendPHPRequest() {
     if (req.readyState==4 && req.status==200) {
       document.getElementById("Tables").innerHTML=req.responseText;
 	  $("#mainTable").dataTable();
-	  $("#supTable").dataTable()
+	  $("#supTable").dataTable();
     }
   }
 
   req.open('POST', 'AllDB.php', true);
   req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
   req.send();
-  //if (filter === "students")
-  //  req.send("filter=students&type="+type+"&supervisorID="+supervisorID);
-  //else
-  //  req.send("filter="+filter+"&type="+type);
 }
 
 function showStudents(superID){
-    filter = "students";
-    supervisorID = superID
-    sendPHPRequest();
 }
 
 function showAll() {
-  filter = "all";
-  sendPHPRequest();
 }
 
 function deadlines() {
-  filter = "deadlines";
-  sendPHPRequest();
 }
 
 function showUnassessed() {
-  filter = "unassessed";
-  sendPHPRequest();
 }
 
 function showProvisional() {
-  filter = "provisional";
-  sendPHPRequest();
 }
 
 function showSupervisor() {
-  filter = "supervisors";
-  sendPHPRequest();
 }
 
 function showSuspensions() {
-	filter = "suspensions";
-	sendPHPRequest();
+}
+
+function changeFilter(value) {
+  if (value === "All") {
+     $("#mainTable").columns().visible(true);
+  }
+  else if (value === "Masters") {
+    //$("#mainTable").columns(
+  }
+  else if (value === "PhD") {
+     //$("#mainTable").columns().visible(true);
+  }
+  
+//Kill type for both
+  
+//To kill for masters
+//echo "<th> 3 Month Report Deadline </th>";
+//echo "<th> 3 Month Report Submission </th>";
+//echo "<th> 3 Month Report Approval </th>";
+//echo "<th> 8 Month Report Deadline </th>";
+//echo "<th> 8 Month Report Submission </th>";
+//echo "<th> 8 Month Report Approval </th>";
+
+//To kill for PhD
+/*
+echo "<th> Proposal Seminar </th>";
+echo "<th> Work Hours Year 1 </th>";
+echo "<th> Work Hours Year 2 </th>";
+echo "<th> Work Hours Year 3 </th>";
+*/
 }
