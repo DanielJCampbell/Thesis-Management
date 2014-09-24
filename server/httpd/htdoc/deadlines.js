c3620db7dd5aef4bd3634c473db65608c372ac34
Tomas Cantwell <mossman2142@gmail.com>
2014-09-25 11:52:45 +1200

    removed comments in deadlines

--- server/httpd/htdoc/deadlines.js
+++ server/httpd/htdoc/deadlines.js
@@ -1,6 +1,5 @@
 function deadlines(){
 	$.fn.dataTable.ext.search.push(function ( oSettings, aData, iDataIndex ) {
-			//if deadline is past now and the submission is empty show
 		var currentDate = Date.now();
 		if (aData[14] != "") {
 			var propDate = new Date(aData[14]);
