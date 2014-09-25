cebe820c8183114c97d8679bd8553aa3a4bb57cd
Tomas Cantwell <mossman2142@gmail.com>
2014-09-25 12:22:51 +1200

    past deadline and hasn't submitted anything

--- server/httpd/htdoc/deadlines.js
+++ server/httpd/htdoc/deadlines.js
@@ -1,7 +1,7 @@
 function deadlines(){
 	$.fn.dataTable.ext.search.push(function ( oSettings, aData, iDataIndex ) {
 		var currentDate = Date.now();
-		if (aData[14] != "") {
+		if (aData[14] != "" && aData[15] == "") {
 			var propDate = new Date(aData[14]);
 			if(propDate < currentDate){
 				return true;
