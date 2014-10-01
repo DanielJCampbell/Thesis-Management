6871e3154fbbec9a012fc9b373e56291b80cc007
Tomas Cantwell <mossman2142@gmail.com>
2014-10-02 11:54:37 +1300

    fixed typo

--- server/httpd/htdoc/deadlines.js
+++ server/httpd/htdoc/deadlines.js
@@ -2,7 +2,7 @@ function calcDeadlines(calcFrom){
 	var inputDateString = document.getElementById(calcFrom).value;
 	var inputDate = new Date(inputDateString);
 	var partTimeStatus = document.getElementById("partTimeStatus").value;
-	var studentType = document.getElementByID("type").value;
+	var studentType = document.getElementById("type").value;
 	var inputMonth = inputDate.getMonth();
 	var partTimeModifier = 1;
 	if (partTimeStatus === "partTime"){
