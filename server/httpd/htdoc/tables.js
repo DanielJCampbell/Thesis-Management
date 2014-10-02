aa8ba8ee6148e27a15d6daa4f150e85610dfe7e2
samuraiRAZR <alvinpascual21@ymail.com>
2014-10-02 15:11:14 +1300

    Debugging primary supervisor

--- server/httpd/htdoc/tables.js
+++ server/httpd/htdoc/tables.js
@@ -271,7 +271,10 @@ function format(data) {
 	+ "<tr> <td> Part-Time: </td> <td> " + partTimeString + "</td> </tr>"
 	+ "<tr> <td> Scholarship: </td> <td> <input type='text' id='scholarship' name='scholarship' value = '"+data[7]+"'/></tr>"
 	+ workHourString
+	+ "<tr> <td> Debug value of primary percentage: " + data[11].split(" (")[1].substr(0, -2) + "</td></tr>"
 	+ "<tr> <td> Primary Supervisor: </td> <td> <input type = 'text' id = 'pSupervisor' name = 'pSupervisor' value = '" + data[11].split(" (")[0] + "'/></td>"
 		+ "<td> Percentage: </td> <td> <input type = 'number' min = '51' max = '99' id = 'pPercentage' name = 'pPercentage' value = '" + data[11].split(" (")[1].substr(0, -2) + "'/></td></tr>"
+		+ "<tr> <td> Secondary Supervisor: </td> <td> <input type = 'text' id = 'sSupervisor' name = 'sSupervisor' value = '" + data[12].split(" (")[0] + "'/></td>"
+		+ "<td> Percentage: </td> <td> <input type = 'number' min = '0' max = '49' id = 'sPercentage' name = 'sPercentage' value = '" + data[12].split(" (")[1].substr(0, -2) + "'/></td></tr>"
 	+ "</table> </form>";
 }
