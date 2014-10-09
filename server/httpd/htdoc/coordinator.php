8c68885022eb55c1d1c5950a973d7178ce39ea16
Nainesh <nainesh.patel@hotmail.co.nz>
2014-10-09 14:52:34 +1300

    aligning logo and title

--- server/httpd/htdoc/coordinator.php
+++ server/httpd/htdoc/coordinator.php
@@ -9,7 +9,7 @@
 	<script type="text/javascript" charset="utf8" src="https://datatables.net/release-datatables/extensions/ColVis/js/dataTables.colVis.js"></script>
     <script src = "tables.js"></script>
     <div id = "image"> <img src="vuw-logo.png" style="height:75px"> </div>
-    <div> <h1> Thesis Management Staff Page</h1> </div>
+    <h1> Thesis Management Staff Page</h1>
   </head>
 
   <body>
