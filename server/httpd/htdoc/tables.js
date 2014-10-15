efdbbc81ec07947ada7d671e95eb0b869eb8e32c
samuraiRAZR <alvinpascual21@ymail.com>
2014-10-16 11:00:48 +1300

    LOGGING SLIGHTLY LESS THINGS

--- server/httpd/htdoc/tables.js
+++ server/httpd/htdoc/tables.js
@@ -49,16 +49,7 @@ function sendPHPRequest() {
 	      else {
 			// Open this row
 	    	var child = row.child(format(row.data()));
-
-	    	var childSID = jQuery('input:read-only', child);
-	    	var childPS = jQuery('#pSupervisor', child);
-	    	var childSS = jQuery('#sSupervisor', child);
-	    	console.log(childSID);
-	    	console.log(childPS);
-	    	console.log(childSS);
-	    	console.log(childSID.index('input:read-only'));
-	    	console.log(childSID.get(childSID.index('input:read-only')));
-	    	console.log(childSID.get(childSID.index('input:read-only')).value);
+	    	console.log(child);
 
 	    	console.log(child.get(child.index('input:read-only')));
 	    	console.log(child.get(child.index('input:read-only')).value);
